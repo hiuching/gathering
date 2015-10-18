0461079a19920fe75a31322f4770b1478b4822ea
hiuching <comp356>
2015-10-19 01:20:14 +0800

    jhjhjhjhj Signed-off-by: hiuching <comp356>

---
+++ models/file.js
@@ -0,0 +1,13 @@
+﻿var fs = require("fs");
+var File = function (){
+};
+
+File.create = function (options, callback) {
+	options = options || {};
+	var base64Data = options.base64encoded.replace(/^data:image\/png;base64,/, "");
+	fs.writeFile(options.path + '/' + options.name, base64Data, 'base64', function(err) {
+	  callback(err);
+	});
+};
+
+module.exports =  File;
