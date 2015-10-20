bcfa14b94ca0ff1e238c62c87cf87fbccc6c3d72
Jeffee ;D <comp356>
2015-10-20 22:40:01 +0800

    Modified at last meeting

--- models/file.js
+++ models/file.js
@@ -4,7 +4,7 @@ var File = function (){
 
 File.create = function (options, callback) {
 	options = options || {};
-	var base64Data = options.base64encoded.replace(/^data:image\/png;base64,/, "");
+	var base64Data = options.base64encoded.replace(/^data:image\/(jpeg|png);base64,/, "");
 	fs.writeFile(options.path + '/' + options.name, base64Data, 'base64', function(err) {
 	  callback(err);
 	});
